05a7ffeb12e34b851b69e61edc29369b36125763
vfrimpong <78256886+vfrimpong@users.noreply.github.com>
2022-03-18 22:25:37 -0400

    website.main

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const responsive = {
+    0 :{
+        items:1
+    },
+    320 : {
+        items:1
+    },
+
+    560 :{
+        items:2
+    },
+
+    960: {
+        items:3
+    }
+}
+
+
+
+
+$(document).ready(function(){
+
+    $nav=$('.nav');
+
+    $toggleCollapse=$('.toggle-collapse');
+
+
+    /**      click event on toggle menu */
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');        
+    })
+
+
+    //owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        dots: false,
+        nav: true,
+        responsive: responsive
+
+    });
+
+
+
+
+
+});
